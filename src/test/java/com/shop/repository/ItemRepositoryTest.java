526c1fde8c266ec47078a7034fd44685741434b0
sanghyeokYun <dev.shyun@gmail.com>
2022-01-05 00:03:45 +0900

    create item test

---
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test //테스트 단위로 지정, 각 단위마다 필요한 객체를 생성해 지원
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest(){
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setStockNumber(100);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        System.out.println(item.toString());
+    }
+
+
+
+}
