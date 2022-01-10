911458761022ae12738a4af138571e07aca97bc2
sanghyeokYun <dev.shyun@gmail.com>
2022-01-10 23:58:15 +0900

    give shape to login, logout function

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -1,20 +1,42 @@
 package com.shop.config;
 
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    MemberService memberService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception { //Http요청에 대한 보안 설정(페이지 권한 설정)
+        http.formLogin()
+                .loginPage("/members/login") //로그인 페이지 URL 설정
+                .defaultSuccessUrl("/") //로그인 성공시 이동할 URL
+                .usernameParameter("email") //로그인 시 사용할 파라미터 이름 지정
+                .failureUrl("/members/login/error") //로그인 실패 시 이동할 URL
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) //로그아웃 URL 설정
+                .logoutSuccessUrl("/"); //로그아웃 성공 시 이동할 URL 설정
+    }
 
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //Spring Security에서 인증은 AuthenticationManager를 통해 이루어짐
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -3,6 +3,10 @@ package com.shop.service;
 import com.shop.entity.Member;
 import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,8 +14,8 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
-
+public class MemberService implements UserDetailsService {
+//UserDetailService인터페이스 - DB에서 회원 정보를 가져오는 역할을 담당
     private final MemberRepository memberRepository;
 
     public Member saveMember(Member member){
@@ -26,4 +30,20 @@ public class MemberService {
         }
     }
 
+    //로그인 로그아웃기능 구현
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        //회원정보 조회하여 사용자의 정보와 권한을 담고있는 UserDetails 인터페이스를 반환
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()
+                .username(member.getName())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
+    }
 }
